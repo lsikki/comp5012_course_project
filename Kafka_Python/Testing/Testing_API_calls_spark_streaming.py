d2ac468c7e750d72c17aa0bb36ba9f026874efef
Laila Ikki <108148866+lsikki@users.noreply.github.com>
2023-11-16 11:43:23 -0500

    Streaming with spark

---
+++ Kafka_Python/Testing/Testing_API_calls_spark_streaming.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import expr
+
+spark = SparkSession.builder \
+    .appName("KafkaSparkStreamingApp") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
+    .config("spark.hadoop.home.dir", "C:\spark\hadoop") \
+    .getOrCreate()
+
+
+kafka_bootstrap_servers = "10.100.129.65:9092"
+kafka_topic = "sparktest"
+
+df = ( spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+    .option("subscribe", kafka_topic) \
+    .load() )
+
+query = df \
+    .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+    .writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .start()
+
+query.awaitTermination()
