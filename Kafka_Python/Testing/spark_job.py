3a01e6b63095d51ebb9a786ad720a7ed41b626f4
lsikki <108148866+lsikki@users.noreply.github.com>
2023-11-27 08:28:03 -0500

    Now instead of appending to console, it writes to a file

--- Kafka_Python/Testing/spark_job.py
+++ Kafka_Python/Testing/spark_job.py
@@ -13,6 +13,7 @@ spark = SparkSession.builder \
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
     .config("spark.hadoop.home.dir", "C:\spark\hadoop") \
     .config("spark.sql.adaptive.enabled", "false") \
+    .config("spark.sql.streaming.checkpointLocation", "file:///C:/sparkcheckpoints") \
     .getOrCreate()
 
 kafka_bootstrap_servers = '10.0.0.137:9092,10.0.0.120:9092'
@@ -26,14 +27,15 @@ df = (spark.readStream
 
 result_df = df.withColumn("udf_test_value", my_udf(col("value")))
 
+# Specify the path to the output directory
+output_path = "file:///C:/sparkoutput"  # Update the path as needed
 
 query = result_df \
     .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "udf_test_value") \
     .writeStream \
     .outputMode("append") \
-    .option('truncate', "false") \
-    .format("console") \
+    .format("json") \
+    .option("path", output_path) \
     .start()
 
 query.awaitTermination()
-
