6df0fb100db7828bc0a04b8727756353336786da
lsikki <108148866+lsikki@users.noreply.github.com>
2023-11-28 14:32:02 -0500

    push

--- Kafka_Python/Testing/spark_job.py
+++ Kafka_Python/Testing/spark_job.py
@@ -1,12 +1,64 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 from pyspark.sql.functions import udf
-from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import pandas_udf
+from pyspark.sql.types import StringType
+import numpy as np
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
 
-def my_udf_function(value):
-    return len(str(value))
+def my_udf_function(key,value):
+    #print(key)
+    #print(value)
 
-my_udf = udf(my_udf_function, IntegerType())
+    #convert the byte arrays to numpy array and make a dataframe
+    numpy_key = np.frombuffer(key,dtype=np.uint8)
+    numpy_value = np.frombuffer(value,dtype=np.uint8)
+    print(numpy_key)
+
+    #preprocess the data
+    df = pd.DataFrame({'Date': numpy_key.flatten(), 'Close': numpy_value.flatten()})
+    #df['Date'] = df['Date'].apply(lambda x: x.timestamp())
+    df['Close'] = df['Close'].astype(int)
+
+    #now we have date and closing price we will process sequences
+    sequences = []
+    target_prices = []
+    dates = []
+
+    #setting length of our seq = 5
+    length = 5
+    for i in range(len(df) - length):
+      seq = df['Close'][i:i+length]
+      date = df['Date'][i+length]
+      target_price = df['Close'][i+length]
+      dates.append(date)
+      sequences.append(seq)
+      target_prices.append(target_price)
+    dates = np.array(dates)
+    seq = np.array(sequences)
+    tar = np.array(target_prices)
+
+    split = int(len(seq)*0.7)
+    dates_train, dates_test, X_train, X_test, y_train, y_test = dates[:split], dates[split:], seq[:split], seq[split:], tar[:split], tar[split:]
+
+    model = Sequential()
+    model.add(LSTM(50, activation = 'relu', input_shape = (5, 1)))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
+    X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
+    print(y_train)
+
+    # Train the model
+    history = model.fit(X_train, y_train, epochs=20, batch_size=32, validation_data=(X_test, y_test))
+    y_pred = model.predict(X_test)
+
+    return y_pred
+
+my_udf = udf(my_udf_function, StringType())
 
 spark = SparkSession.builder \
     .appName("KafkaSparkStreamingApp") \
@@ -14,10 +66,11 @@ spark = SparkSession.builder \
     .config("spark.hadoop.home.dir", "C:\spark\hadoop") \
     .config("spark.sql.adaptive.enabled", "false") \
     .config("spark.sql.streaming.checkpointLocation", "file:///C:/sparkcheckpoints") \
+    .config("spark.sql.execution.arrow.enabled", "true") \
     .getOrCreate()
 
-kafka_bootstrap_servers = '10.0.0.137:9092,10.0.0.120:9092'
-kafka_topic = "sparkstreamtestyahooudf"
+kafka_bootstrap_servers = '172.17.12.108:9092'
+kafka_topic = "amazon"
 
 df = (spark.readStream
       .format("kafka")
@@ -25,17 +78,19 @@ df = (spark.readStream
       .option("subscribe", kafka_topic)
       .load())
 
-result_df = df.withColumn("udf_test_value", my_udf(col("value")))
-
+result_df = df.withColumn("predicted_price",my_udf(col("key"),col("value")))
+#result_df.show()
+#df.withColumn("predicted_values", my_udf(spark.create_DataFrame(col("key"),col("value"))))
+#df.select(col['key'],col['value'],my_udf_function)
 # Specify the path to the output directory
 output_path = "file:///C:/sparkoutput"  # Update the path as needed
 
 query = result_df \
-    .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "udf_test_value") \
+    .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)", "predicted_price") \
     .writeStream \
     .outputMode("append") \
-    .format("json") \
-    .option("path", output_path) \
+    .option('truncate', "false") \
+    .format("console") \
     .start()
 
 query.awaitTermination()
--- Kafka_Python/Testing/yahoo_producer.py
+++ Kafka_Python/Testing/yahoo_producer.py
@@ -2,17 +2,49 @@ import json
 from kafka import KafkaProducer
 import yfinance as yf
 from datetime import datetime, timedelta
+import requests
 
 kafka_config = {
-    'bootstrap_servers': '10.0.0.137:9092,10.0.0.120:9092',
+    'bootstrap_servers': '172.17.12.108:9092',
     'client_id': 'laila-python-producer',
     'value_serializer': lambda v: json.dumps(v).encode('utf-8'),
     'key_serializer': lambda k: str(k).encode('utf-8') if k is not None else None
 }
-
-topic_name = 'sparkstreamtestyahooudf'
-
 producer = KafkaProducer(**kafka_config)
+
+#topic_name = 'sparkstreamtestyahooudf'
+topic_microsoft = 'microsoft'
+
+topic_amazon = 'amazon'
+api_key_amzn = 'P544V2LKQW6KH46R'
+symbol_amzn = 'AMZN'
+endpoint = 'https://www.alphavantage.co/query'
+function = 'TIME_SERIES_DAILY'
+output_size = 'full'
+# Construct the API URL
+url = f'{endpoint}?function={function}&symbol={symbol_amzn}&outputsize={output_size}&apikey={api_key_amzn}'
+
+# Make the API request
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the JSON response
+    data = response.json()
+
+    # Access the time series data
+    time_series_data = data['Time Series (Daily)']
+    #print(time_series_data)
+    
+    # Sending the first few entries for now
+    for date, values in list(time_series_data.items()):
+        print(f'Date: {date}, Closing Price: {values["4. close"]}')
+        producer.send(topic_amazon, key=date, value=values["4. close"])
+else:
+    print(f'Error: {response.status_code}, {response.text}')
+
+
+'''
 
 yf_data = yf.download('AZN', start='2023-01-01', end='2023-02-01')
 yf_data = yf_data.reset_index()
@@ -27,5 +59,6 @@ messages = yf_data.to_dict(orient='index')
 for key, value in messages.items():
     value_without_date = {k: v for k, v in value.items() if k != 'Date'}
     producer.send(topic_name, key=value['Date'], value=value_without_date)
+'''
 
 producer.close()
