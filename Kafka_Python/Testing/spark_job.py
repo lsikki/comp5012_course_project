d57c659cd9324ee29c0874c8b9880953a163fee2
SamiaIshaqML <145932439+SamiaIshaqML@users.noreply.github.com>
2023-11-30 20:27:01 -0500

    Preprocessing the csv data for amazon - error for date value

--- Kafka_Python/Testing/spark_job.py
+++ Kafka_Python/Testing/spark_job.py
@@ -4,89 +4,72 @@ from pyspark.sql.types import StringType, DoubleType, StructType, StructField, D
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
-from sklearn.metrics import mean_squared_error
 from datetime import datetime
-import pandas as pd
 
-# Function to create sequences for LSTM
 def create_sequences(stock_data, length):
     sequences = []
     target_prices = []
     dates = []
 
     for i in range(len(stock_data) - length):
-        seq = stock_data['processed_closing_prices'][i:i+length]
-        date = stock_data['processed_dates'][i+length]
-        target_price = stock_data['processed_closing_prices'][i+length]
-        
-        # Log the 'Close' values here
-        print(f"Date: {date}, Close: {seq}")
-
+        seq = stock_data['Close'][i:i+length]
+        date = stock_data['Date'][i+length]
+        target_price = stock_data['Close'][i+length]
         dates.append(date)
         sequences.append(seq)
         target_prices.append(target_price)
 
     return np.array(dates), np.array(sequences), np.array(target_prices)
 
+def LSTM_Model(spark, df):
+    if len(df) == 0:
+        print("DataFrame is empty.")
+        return
 
-# LSTM Model
-def LSTM_Model(df):
-    if len(df) != 0:
-        print("printing length: " + str(len(df)))
-        dates,seq,tar = create_sequences(df, 5)
-        
-        #print(dates, seq, tar)
+    dates, seq, tar = create_sequences(df, 5)
 
-        split = int(len(seq)*0.7)
-        dates_train, dates_test, X_train, X_test, y_train, y_test = dates[:split], dates[split:], seq[:split], seq[split:], tar[:split], tar[split:]
+    split = int(len(seq) * 0.7)
+    dates_train, dates_test, X_train, X_test, y_train, y_test = (
+        dates[:split],
+        dates[split:],
+        seq[:split],
+        seq[split:],
+        tar[:split],
+        tar[split:],
+    )
 
-        model = Sequential()
-        model.add(LSTM(50, activation = 'relu', input_shape = (5, 1)))
-        model.add(Dense(1))
-        model.compile(optimizer='adam', loss='mean_squared_error')
+    model = Sequential()
+    model.add(LSTM(50, activation='relu', input_shape=(5, 1)))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mean_squared_error', run_eagerly=True)  
 
-        X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], 1))
-        X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], 1))
-        #print(y_train)
+    X_train = np.expand_dims(X_train, axis=1)
+    X_test = np.expand_dims(X_test, axis=1)
 
-        history = model.fit(X_train, y_train, epochs=20, batch_size=32, validation_data=(X_test, y_test))    
+    X_train_df = spark.createDataFrame([(a.tolist(), b.tolist()) for a, b in zip(X_train, y_train)], ["features", "label"])
+    X_test_df = spark.createDataFrame([(a.tolist(), b.tolist()) for a, b in zip(X_test, y_test)], ["features", "label"])
 
-        import matplotlib.pyplot as plt
-        y_pred = model.predict(X_test)
-        mse = mean_squared_error(y_test, y_pred)
-        print(mse)
-        
-        plt.plot(dates_test, y_pred)
-        plt.plot(dates_test, y_test)
-        plt.savefig('example_plot.png')
-        plt.show()
-    else:
-        y_pred = []
+    history = model.fit(X_train_df, epochs=20, batch_size=32, validation_data=X_test_df)
+    y_pred = model.predict(X_test)
+
     return y_pred
 
-
-# UDF to process dates
 def dates_udf(key):
     key_str = key.decode('utf-8')
     date = datetime.strptime(key_str, "%Y-%m-%d")
-    return date
+    return key_str
 
-# UDF to process prices
 def prices_udf(value):
     value_str = value.decode('utf-8')
-    x = value_str
-    
     try:
-        return x
+        price = float(value_str)
+        return price
     except ValueError:
-        return None
+        return None  
 
-
-# UDF registration
 reg_dates_udf = udf(dates_udf, DateType())
 reg_prices_udf = udf(prices_udf, DoubleType())
 
-# Spark Session creation
 spark = SparkSession.builder \
     .appName("KafkaSparkStreamingApp") \
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0") \
@@ -96,25 +79,22 @@ spark = SparkSession.builder \
     .config("spark.sql.execution.arrow.enabled", "true") \
     .getOrCreate()
 
-# Kafka setup
-kafka_bootstrap_servers = '172.17.12.108:9092'
+kafka_bootstrap_servers = '10.0.0.10:9092'
 kafka_topic = "amazon"
 
-# Read data from Kafka
 df = (spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", kafka_bootstrap_servers)
       .option("subscribe", kafka_topic)
       .load())
 
-df.printSchema()
-
-# Process data with UDFs
 result = df.withColumn("processed_dates", reg_dates_udf(col("key"))) \
-            .withColumn("processed_closing_prices", col("value"))
+            .withColumn("processed_closing_prices", reg_prices_udf(col("value")))
+
 
 def process_batch(batch_df, batch_id):
-    pandas_df = batch_df.toPandas()    
+    print(batch_df)
+    pandas_df = batch_df  
     pandas_df['processed_closing_prices'] = pandas_df['processed_closing_prices'].apply(
         lambda x: float(x.decode('utf-8').replace('"', '')) if isinstance(x, bytes) else x
     )
@@ -122,17 +102,14 @@ def process_batch(batch_df, batch_id):
 
     pandas_filtered = pandas_df[columns_to_keep]
     
-    y_pred = LSTM_Model(pandas_filtered)
-    print(f"Batch {batch_id} - LSTM Predictions: {y_pred}")
+    y_pred = LSTM_Model(spark, pandas_filtered)
+    print(f"Batch {batch_id} - LSTM Predictions: {y_pred}")
 
-    
 
-# Start the streaming query
 query = result \
     .writeStream \
     .outputMode("append") \
     .foreachBatch(process_batch) \
-    .trigger(processingTime="50 seconds") \
     .start()
 
 query.awaitTermination(timeout=30)
